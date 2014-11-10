eb2184d07ec871aef4b420fda6244128163a3ca6
Ashton French <bendthesky@yahoo.com>
2014-11-10 01:07:53 -0600

    Party Invites App

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int Hour = DateTime.Now.Hour;
+            ViewBag.Pingas = Hour < 12 ? "PINGAS" : "Pingas";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse response)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Email response to party organizer
+                return View("Thanks", response);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
